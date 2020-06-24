046e3594eabcda8cda1408cd8c5c3eb5bc5d0f1b
sofki <skyriakidi@gmail.com>
2020-06-24 16:03:08 +0300

    created files

---
+++ guessinggame.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# File: guessingame.sh
+
+files=$(ls -1 | wc -l)
+
+function guess {
+	echo "How many files are in the working directory?"
+	read input
+}
+
+
+while [[ $input -ne $files ]]
+do
+	guess
+	if [[ $input -lt $files ]]
+	then
+		echo "Unfortunately this is too low"
+	elif [[ $input -gt $files ]]
+	then
+		echo "Unfortunately that's too high"
+	else
+		echo "That is correct!"
+	fi 
+done
+
